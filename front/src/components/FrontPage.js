6aef9f937919ad853235c3b5870a779d3a8d1000
bukomp <edvard@shalaev.com>
2019-05-10 09:36:05 +0300

    play/pause

--- front/src/components/FrontPage.js
+++ front/src/components/FrontPage.js
@@ -46,7 +46,7 @@ const FrontPage = (props) => {
   };
 
   const likedSong = () => {
-    if(id !== null && id !== undefined && id !== "undefined" && id !== "null")addFavourite(window.localStorage.getItem('token'), music.musicPlayingId).this(res => console.log(res)).catch(err => console.log(err));
+    if(music.musicPlayingId !== null && music.musicPlayingId !== undefined && music.musicPlayingId !== "undefined" && music.musicPlayingId !== "null")addFavourite(window.localStorage.getItem('token'), music.musicPlayingId).this(res => console.log(res)).catch(err => console.log(err));
   };
 
     return (
